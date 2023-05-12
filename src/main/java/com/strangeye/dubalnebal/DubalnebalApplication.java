272135573706f56d800333bfcabfa5a0f7a237ce
Sangho <istrangeho@gmail.com>
2023-05-12 17:26:11 +0900

    Initial commit

---
+++ src/main/java/com/strangeye/dubalnebal/DubalnebalApplication.java
@@ -0,0 +1,13 @@
+package com.strangeye.dubalnebal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubalnebalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubalnebalApplication.class, args);
+	}
+
+}
