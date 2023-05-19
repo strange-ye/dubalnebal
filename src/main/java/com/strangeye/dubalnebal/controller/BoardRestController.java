eba7f931feced5446de9c931deb3827c68295df5
youngeun_kim <leongeun@naver.com>
2023-05-19 09:11:36 +0900

    20230519_오전점검

--- src/main/java/com/strangeye/dubalnebal/controller/BoardRestController.java
+++ src/main/java/com/strangeye/dubalnebal/controller/BoardRestController.java
@@ -1,4 +1,18 @@
 package com.strangeye.dubalnebal.controller;
 
+import com.strangeye.dubalnebal.service.BoardService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@Api(tags = "게시판 컨트롤러")
 public class BoardRestController {
+
+	@Autowired
+	private BoardService boardService;
+
+
 }
--- src/main/java/com/strangeye/dubalnebal/dto/Board.java
+++ src/main/java/com/strangeye/dubalnebal/dto/Board.java
@@ -1,4 +1,11 @@
 package com.strangeye.dubalnebal.dto;
 
+import io.swagger.annotations.ApiModel;
+
+@ApiModel(value = "후기 게시판", description = "게시판 정보")
 public class Board {
+	private int id;
+	private String title;
+	private String content;
+	private int viewCnt;
 }
