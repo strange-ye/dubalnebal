fa3614fa1cbf9b10c37dde2b593400ee1dc27f5e
youngeun_kim <leongeun@naver.com>
2023-05-19 17:53:36 +0900

    20230519_오전점검

--- src/main/java/com/strangeye/dubalnebal/controller/BoardRestController.java
+++ src/main/java/com/strangeye/dubalnebal/controller/BoardRestController.java
@@ -1,18 +1,35 @@
 package com.strangeye.dubalnebal.controller;
 
+import com.strangeye.dubalnebal.dto.Board;
 import com.strangeye.dubalnebal.service.BoardService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 @Api(tags = "게시판 컨트롤러")
 public class BoardRestController {
 
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+
 	@Autowired
 	private BoardService boardService;
 
+	@GetMapping("/board")
+	public ResponseEntity<?> list(){
+		return new ResponseEntity<>("hi", HttpStatus.OK);
+	}
+
+//	@PostMapping("/upload")
+//	public ResponseEntity<?> upload(@RequestParam Board board) {
+//		BoardService.insert(board)
+//		return new ResponseEntity<>()
+//	}
 
 }
--- src/main/java/com/strangeye/dubalnebal/service/BoardServiceImpl.java
+++ src/main/java/com/strangeye/dubalnebal/service/BoardServiceImpl.java
@@ -1,7 +1,52 @@
 package com.strangeye.dubalnebal.service;
 
+import com.strangeye.dubalnebal.dao.BoardDao;
+import com.strangeye.dubalnebal.dto.Board;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BoardServiceImpl {
+public class BoardServiceImpl implements BoardService{
+
+	@Autowired
+	private BoardDao boardDao;
+
+//	@Autowired
+//	public void setBoardDao(BoardDao boardDao){
+//		this.boardDao = boardDao;
+//	}
+
+	@Override
+	public List<Board> getBoardList(){
+		return  boardDao.selectAll();
+	}
+
+	@Override
+	public Board detailBoard(int board_id) {
+		return null;
+	}
+
+	@Override
+	public void writeBoard(Board board) {
+
+	}
+
+	@Override
+	public void removeBoard(int board_id) {
+
+	}
+
+	@Override
+	public void modifyBoard(Board board) {
+
+	}
+
+	@Override
+	public void updateViewCnt(int board_id) {
+
+	}
+
+
 }
