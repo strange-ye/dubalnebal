49590d79cf0de322b403183b686e4f134f3636fe
Sangho <istrangeho@gmail.com>
2023-05-21 16:23:40 +0900

    sangho 2023 05 21 16:22. Add some comments and Course Dto.

--- src/main/java/com/strangeye/dubalnebal/dto/Course.java
+++ src/main/java/com/strangeye/dubalnebal/dto/Course.java
@@ -1,4 +1,14 @@
 package com.strangeye.dubalnebal.dto;
 
 public class Course {
+
+	private int course_id;
+	private String course_name;
+	private String course_path;
+	private String course_locaiton;
+	private String course_level;
+	private String course_km;
+	private String course_explain;
+	private String course_time;
+
 }
